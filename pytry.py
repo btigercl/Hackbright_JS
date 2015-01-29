1ab850090ee270e5f3a06bbead8e37b20c6c909b
Hackbright Student <info@hackbrightacademy.com>
2015-01-28 16:00:56 -0800

    Completed Exercise 2

--- pytry.py
+++ pytry.py
@@ -1,5 +1,17 @@
-sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
-print sum
+def fibonacci_set(max):
+    """Return list of fibonacci numbers going up to but not beyond max."""
+    if max > 1:
+        fib_list = [1]
+        current_fib = 1
+        while current_fib < max:
+            fib_list.append(current_fib)
+            current_fib = fib_list[-1] + fib_list[-2]
+        return fib_list
+    else:
+        return [1, 1]
+
+def is_even(n):
+    """Is number even?"""
+    return n % 2 == 0
+
+print sum(filter(is_even, fibonacci_set(4000000)))
